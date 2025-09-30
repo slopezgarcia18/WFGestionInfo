77ad71d0dff203e56e6bd321f6cf3897ac2ae72e
SergioLopez\sergi <slopezgarcia188@gmail.com>
2025-09-30 10:32:30 +0200

    Creada la ventana preferencias

--- WFGestionInfo/Form1.cs
+++ WFGestionInfo/Form1.cs
@@ -16,5 +16,10 @@ namespace WFGestionInfo
         {
             InitializeComponent();
         }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
