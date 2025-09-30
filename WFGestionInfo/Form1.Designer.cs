b0aaa79466abcc7fccb324f665ce66f991795d9b
SergioLopez\sergi <slopezgarcia188@gmail.com>
2025-09-30 16:49:33 +0200

    Arreglando ventana preferencias

--- WFGestionInfo/Form1.Designer.cs
+++ WFGestionInfo/Form1.Designer.cs
@@ -33,17 +33,17 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tpDatos = new System.Windows.Forms.TabPage();
             this.tpPreferen = new System.Windows.Forms.TabPage();
+            this.label5 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.clbListaHobbies = new System.Windows.Forms.CheckedListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.tpVisual = new System.Windows.Forms.TabPage();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.clbListaHobbies = new System.Windows.Forms.CheckedListBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
-            this.label5 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tpPreferen.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
@@ -104,6 +104,90 @@
             this.tpPreferen.Text = "Preferencias";
             this.tpPreferen.UseVisualStyleBackColor = true;
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(598, 241);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(167, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Valoración del servicio";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(601, 279);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(199, 56);
+            this.trackBar1.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(747, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Pais de residencia: ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Noruega",
+            "España",
+            "Alemania",
+            "Italia",
+            "Paises Bajos",
+            "Francia",
+            "Bélgica",
+            "Suiza",
+            "Letonia",
+            "Brasil"});
+            this.comboBox1.Location = new System.Drawing.Point(750, 85);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(204, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(416, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Hobbies";
+            // 
+            // clbListaHobbies
+            // 
+            this.clbListaHobbies.FormattingEnabled = true;
+            this.clbListaHobbies.Items.AddRange(new object[] {
+            "Pintar",
+            "Cantar",
+            "Senderismo ",
+            "Pescar",
+            "Programar",
+            "Ajedrez",
+            "Artes marciales",
+            "Gym"});
+            this.clbListaHobbies.Location = new System.Drawing.Point(342, 66);
+            this.clbListaHobbies.Name = "clbListaHobbies";
+            this.clbListaHobbies.Size = new System.Drawing.Size(239, 123);
+            this.clbListaHobbies.TabIndex = 1;
+            this.clbListaHobbies.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(611, 378);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(167, 20);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "¿Suscribirse al boletín?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // tpVisual
             // 
             this.tpVisual.Location = new System.Drawing.Point(4, 25);
@@ -139,90 +223,6 @@
             this.button3.TabIndex = 5;
             this.button3.Text = "Salir";
             this.button3.UseVisualStyleBackColor = true;
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(611, 386);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(167, 20);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "¿Suscribirse al boletín?";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // clbListaHobbies
-            // 
-            this.clbListaHobbies.FormattingEnabled = true;
-            this.clbListaHobbies.Items.AddRange(new object[] {
-            "Pintar",
-            "Cantar",
-            "Senderismo ",
-            "Pescar",
-            "Programar",
-            "Ajedrez",
-            "Artes marciales",
-            "Gym"});
-            this.clbListaHobbies.Location = new System.Drawing.Point(342, 66);
-            this.clbListaHobbies.Name = "clbListaHobbies";
-            this.clbListaHobbies.Size = new System.Drawing.Size(239, 123);
-            this.clbListaHobbies.TabIndex = 1;
-            this.clbListaHobbies.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(416, 29);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(66, 16);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Hobbies";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Noruega",
-            "España",
-            "Alemania",
-            "Italia",
-            "Paises Bajos",
-            "Francia",
-            "Bélgica",
-            "Suiza",
-            "Letonia",
-            "Brasil"});
-            this.comboBox1.Location = new System.Drawing.Point(750, 85);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(204, 24);
-            this.comboBox1.TabIndex = 3;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(747, 29);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(145, 16);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Pais de residencia: ";
-            // 
-            // trackBar1
-            // 
-            this.trackBar1.Location = new System.Drawing.Point(601, 279);
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(199, 56);
-            this.trackBar1.TabIndex = 5;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(598, 241);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(167, 16);
-            this.label5.TabIndex = 6;
-            this.label5.Text = "Valoración del servicio";
             // 
             // Form1
             // 
